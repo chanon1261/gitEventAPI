7ad43b29a77b2a107f514c628b13cfab7531bb11
M416 <chanon_w@cmu.ac.th>
2019-05-13 17:19:52 +0700

    complete

--- app/src/main/java/com/example/android/test/adapter/EvenGitAdapter.kt
+++ app/src/main/java/com/example/android/test/adapter/EvenGitAdapter.kt
@@ -5,8 +5,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.android.databinding.library.baseAdapters.BR
+import com.example.android.test.MainApp
 import com.example.android.test.databinding.EventItemBinding
 import com.example.android.test.model.Event
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.event_item.view.*
 
 
 class EvenGitAdapter(val context: Context) : RecyclerView.Adapter<EvenGitAdapter.EventdapterViewHolder>() {
@@ -40,6 +43,8 @@ class EvenGitAdapter(val context: Context) : RecyclerView.Adapter<EvenGitAdapter
             binding.setVariable(BR.login, item.actor.login)
             binding.setVariable(BR.display, item.actor.display_login)
             binding.setVariable(BR.url, item.actor.url)
+            Picasso.with(MainApp.instance.applicationContext).load(item.actor.avatar_url).into(itemView.img)
+
         }
 
     }
